7d5143d220ccb2bee9e894d20e07e94562a76005
themycode <icloudsman@mail.com>
2026-03-25 11:37:29 +0800

    fix

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,21 +1,24 @@
 <template>
-  <DefaultLayout />
-  <Giscus
-    v-if="showComments"
-    repo="themycode/themycode.github.io"
-    repo-id="MDEwOlJlcG9zaXRvcnkxNjQ1ODkxOTU="
-    category="Announcements"
-    category-id="DIC_kwDOCc9ui84C5HGB"
-    mapping="pathname"
-    strict="0"
-    reactions-enabled="1"
-    emit-metadata="0"
-    input-position="bottom"
-    theme="preferred_color_scheme"
-    lang="zh-CN"
-    crossorigin="anonymous"
-    style="max-width: 900px; margin: 0 auto; padding: 20px"
-  />
+  <Layout>
+    <template #doc-after>
+      <Giscus
+        v-if="showComments"
+        repo="themycode/themycode.github.io"
+        repo-id="MDEwOlJlcG9zaXRvcnkxNjQ1ODkxOTU="
+        category="Announcements"
+        category-id="DIC_kwDOCc9ui84C5HGB"
+        mapping="pathname"
+        strict="0"
+        reactions-enabled="1"
+        emit-metadata="0"
+        input-position="bottom"
+        theme="preferred_color_scheme"
+        lang="zh-CN"
+        crossorigin="anonymous"
+        style="max-width: 900px; margin: 0 auto; padding: 20px"
+      />
+    </template>
+  </Layout>
 </template>
 
 <script setup lang="ts">
@@ -24,7 +27,6 @@ import { useRoute } from 'vitepress'
 import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Giscus from '@giscus/vue'
-import DefaultLayout from 'vitepress/theme'
 const { Layout } = DefaultTheme
 
 const route = useRoute()
